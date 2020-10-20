3e3d61f2dc5ce6f752b22e24e6e1a6d5be09c467
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-10-19 17:00:36 -0700

    First Commit
    Base file & Read Me instructions for editing https://infinitecustoms.dev/ and https://infinitecustoms.dev/shop

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require './src/php/head.html';?>
+</head>
+
+<body id="top">
+
+    <!-- Navigation -->
+    <?php require './src/php/navigation.html';?>
+    <main>
+        <!-- Master Header  -->
+        <?php require './src/php/masterheader.html';?>
+        <!-- Icon Grid -->
+        <?php require './src/php/icon-grid.html';?>
+        <!-- show case  -->
+        <?php require './src/php/showcase.html';?>
+        <!-- Testimonials  -->
+        <?php require './src/php/testimonials.html';?>
+        <!-- call to action  -->
+        <?php require './src/php/calltoaction.html';?>
+        <!-- footer  -->
+        <?php require './src/php/footer.html';?>
+        <!-- scripts -->
+        <?php require_once './src/php/scripts.html';?>
+    </main>
+
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Basic Information
+> When editing this file structor you don't edit the "index.php" structure, as it is preconfigured and what you want isn't in there anyways.
+
+# Following File Structor 
+> To edit the site you will go to **`./src/php/<filename>`** to edit the site, dont mess with anything in **`<>`** tags or you will have to pay for me to fix this file structure. 
